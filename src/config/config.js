06a6ba1e8d778348fad4532b3e478d3b88620c3b
Justin Mann <justin.mann@gmail.com>
2020-07-07 19:35:12 -0700

    update firebase

--- src/config/config.js
+++ src/config/config.js
@@ -17,31 +17,26 @@ const LPAsync = Loadable({
 
 const config = {
   firebase_config: {
-    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
-    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-    projectId: 'react-most-wanted-3b1b2',
-    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-    messagingSenderId: '258373383650',
-    appId: '1:258373383650:web:b49ad5dd28da999a',
+    apiKey: "AIzaSyABMNzrcUScVLTH4GH2cHiLdOVsfEZFlbo",
+    authDomain: "built-8f8b6.firebaseapp.com",
+    databaseURL: "https://built-8f8b6.firebaseio.com",
+    projectId: "built-8f8b6",
+    storageBucket: "built-8f8b6.appspot.com",
+    messagingSenderId: "910971953299",
+    appId: "1:910971953299:web:3cb2f2825f97e64d46ed66",
+    measurementId: "G-ZKF4YKPEG8"
   },
   firebase_config_devp: {
-    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
-    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-    projectId: 'react-most-wanted-3b1b2',
-    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-    messagingSenderId: '258373383650',
-    appId: '1:258373383650:web:b49ad5dd28da999a',
+    apiKey: "AIzaSyABMNzrcUScVLTH4GH2cHiLdOVsfEZFlbo",
+    authDomain: "built-8f8b6.firebaseapp.com",
+    databaseURL: "https://built-8f8b6.firebaseio.com",
+    projectId: "built-8f8b6",
+    storageBucket: "built-8f8b6.appspot.com",
+    messagingSenderId: "910971953299",
+    appId: "1:910971953299:web:3cb2f2825f97e64d46ed66",
+    measurementId: "G-ZKF4YKPEG8"
   },
   firebase_config_dev: {
-    apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
-    authDomain: 'react-most-wanted-dev.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
-    projectId: 'react-most-wanted-dev',
-    storageBucket: 'react-most-wanted-dev.appspot.com',
-    messagingSenderId: '70650394824',
-    appId: '1:70650394824:web:7cd3113c37741efc',
   },
   firebase_providers: [
     'google.com',
