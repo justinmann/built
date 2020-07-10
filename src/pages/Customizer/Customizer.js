25061868cc136df5b91b34368484864420b5012d
Justin Mann <justin.mann@gmail.com>
2020-07-09 21:58:40 -0700

    add accordion

--- src/pages/Customizer/Customizer.js
+++ src/pages/Customizer/Customizer.js
@@ -6,6 +6,24 @@ import { injectIntl } from 'react-intl'
 import Scrollbar from 'rmw-shell/lib/components/Scrollbar'
 import { Tabs, Tab } from '@material-ui/core'
 import TabPanel from '../../components/Controls/TabPanel'
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
 
 const Customizer=({intl}) =>{
     const [value, setValue] = useState(0);
@@ -13,6 +31,8 @@ const Customizer=({intl}) =>{
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
+        
+    const classes = useStyles();
     
     return (
       <Activity
@@ -33,41 +53,58 @@ const Customizer=({intl}) =>{
           <Tab label="Interior" value={1} />
         </Tabs>
         <TabPanel value={value} index={0}>
-          Exterior
+          <Grid container>
+            <Grid item xs={3}>  
+              <Paper elevation={3}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography className={classes.heading}>Accordion 1</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                      sit amet blandit leo lobortis eget.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="panel2a-header"
+                  >
+                    <Typography className={classes.heading}>Accordion 2</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                      sit amet blandit leo lobortis eget.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion disabled>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3a-content"
+                    id="panel3a-header"
+                  >
+                    <Typography className={classes.heading}>Disabled Accordion</Typography>
+                  </AccordionSummary>
+                </Accordion>
+              </Paper>
+            </Grid>
+            <Grid item xs={9}>
+              Exterior
+            </Grid>
+          </Grid>
         </TabPanel>
         <TabPanel value={value} index={1}>
           Interior
         </TabPanel>
-        <Scrollbar>
-          <div
-            style={{
-              margin: 5,
-              display: 'flex',
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: '100%',
-              marginTop: 50
-            }}
-          >
-          </div>
-
-          <br />
-          <div
-            style={{
-              margin: 5,
-              display: 'flex',
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: '100%',
-              marginTop: 50
-            }}
-          >
-          </div>
-        </Scrollbar>
       </Activity>
     )
 }
